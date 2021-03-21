a335e592c67837d3244f0ae1e4f97ec3d925af89
Shanen Cross <shanencross@yahoo.com>
2021-03-21 14:42:49 -0700

    Add Pokemon card class
    Co-authored-by: Theron Packus <tlpackus@gmail.com>

---
+++ src/pokemon-card.js
@@ -0,0 +1,9 @@
+export default class PokemonCard {
+	constructor(name, hp, types, attacks, weaknesses) {
+		this.name = name;
+		this.hp = hp;
+		this.types = types;
+		this.attacks = attacks;
+		this.weaknesses = weaknesses;
+	}
+}
