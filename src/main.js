653f4031933d7851945a31fb9c83ee24a0b015eb
Shanen Cross <shanencross@yahoo.com>
2021-03-21 11:27:05 -0700

    Test out Pokemon TCG API, making sure it works
    Co-authored-by: Theron Packus <tlpackus@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+let promise = fetch(`https://api.pokemontcg.io/v2/cards?q=name:Mewtwo`)
+	.then(function(response) {
+		if (!response.ok) {
+			throw Error(response.statusText);
+		}
+		return response.json();
+	})
+	.catch(function(error) {
+		return error;
+	});
+
+promise.then(function(response) {
+	if (response.data) {
+		console.log(response);
+	}
+});
