1a7b7bdef9540adf1423ff93065f85608948c5b5
Shanen Cross <shanencross@yahoo.com>
2021-03-21 14:43:52 -0700

    Refactor and add function for making card object from data
    Co-authored-by: Theron Packus <tlpackus@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,13 +3,32 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import PokemonService from './pokemon-service.js';
+import PokemonCard from './pokemon-card.js';
 
-function showPokemonData(response) {
-	if (response.data) {
-		$('#showImage').append(`<img src='${response.data[0].images.small}'>`);
-	}
+function makeCardFromData(data) {
+	const name = data.name;
+	const hp = data.hp;
+	const types = data.types;
+	const attacks = data.attacks;
+	const weaknesses = data.weaknesses;
+
+	return new PokemonCard(name, hp, types, attacks, weaknesses);
 }
 
-PokemonService.getPokemonData("Mewtwo").then(function(response) {
-	showPokemonData(response);
+function showPokemonImage(data) {
+	$('#showImage').append(`<img src='${data[0].images.small}'>`);
+}
+
+$('#searchButton').click(function(event) {
+	event.preventDefault();
+
+	const pokemonName = $('#name').val();
+	$('#name').val('');
+	
+	PokemonService.getPokemonData(pokemonName).then(function(response) {
+		if (response.data) {
+			showPokemonImage(response.data);
+			let pokemonCard = makeCardFromData(response.data);
+		}
+	});
 });
