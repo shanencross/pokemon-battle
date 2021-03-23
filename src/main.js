51ff8206b3e6db1f6382cc80f56311ccb85b650e
Shanen Cross <shanencross@yahoo.com>
2021-03-22 20:37:14 -0700

    Clear click handlers before adding new ones
    Co-authored-by: Theron Packus <tlpackus@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import PokemonService from './pokemon-service.js';
 import PokemonCard from './pokemon-card.js';
+import PokemonBattle from './pokemon-battle';
+
+let pokemonBattle = new PokemonBattle();
 
 function makeCardFromData(pokemonData) {
 	const name = pokemonData.name;
@@ -29,11 +32,13 @@ $('#searchButton').click(function(event) {
 		if (response.data) {
 			showPokemonImage(response.data[0]);
 			$(".hiddenButton").show();
-
+			$('#selectButton').off('click');
 			$('#selectButton').click(function(event) {
 				let pokemonCard = makeCardFromData(response.data[0]);
+				pokemonBattle.addPokemon(pokemonCard);
 				alert(`${pokemonCard.name}, I choose you!`);
 			});
 		}
 	});
 });
+
